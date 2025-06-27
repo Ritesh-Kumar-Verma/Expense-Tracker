5bce4490a4d2326fd5a00982d8931c2af89a3e2b
Ritesh Kumar Verma <67832781+Ritesh-Kumar-Verma@users.noreply.github.com>
2025-06-27 18:57:24 +0530

    Expense Tracker

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+import "./Sidebar.css";
+const Sidebar = () => {
+  return (
+    <>
+      <div className="sidebar">
+        <div className="profile">
+          <img src={assets.boy} alt="" />
+          <p>Ritesh</p>
+        </div>
+
+        <div className="menu">
+          <div className="home menu-item">
+            <img src={assets.home} alt="" />
+            <p>Home</p>
+          </div>
+          <div className="expense menu-item">
+            <img src={assets.expense} alt="" />
+            <p>Expense</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ src/assets/assets.js
@@ -0,0 +1,12 @@
+import boy from './boy.png'
+import home from './home.svg'
+import calculator from './calculator.png'
+import expense from './expense.svg'
+
+export const assets = {
+    boy,
+    calculator,
+    home,
+    expense
+
+}
