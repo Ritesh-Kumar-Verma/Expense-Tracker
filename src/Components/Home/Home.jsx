5fe24572067aa22051379ac31eccefa740beeeca
Ritesh Kumar Verma <67832781+Ritesh-Kumar-Verma@users.noreply.github.com>
2025-07-02 20:54:51 +0530

    graph container had some issue

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,29 +4,22 @@ import ListOfExpense from "../Cards/ListOfExpense/ListOfExpense";
 import ExpenseChart from "../ExpenseChart/ExpenseChart";
 import OverallExpense from "../OverallExpense/OverallExpense";
 
-const Home = ({expenseList,addExpense}) => {
-  
+const Home = ({ expenseList, addExpense }) => {
   return (
     <div className="main-and-header">
-      <div className="header"></div>
+      <div className="main-header"></div>
 
       <div className="main">
-
         <div className="left">
-
-        <ExpenseCard expenseList={expenseList} addExpense = {addExpense} />
-        <OverallExpense expenseList={expenseList}/>
+          <ExpenseCard expenseList={expenseList} addExpense={addExpense} />
+          <OverallExpense expenseList={expenseList} />
         </div>
         <div className="mid">
-        <ListOfExpense expenseList={expenseList} />
+          <ListOfExpense expenseList={expenseList} />
         </div>
         <div className="right">
-          
-          
-          
           <ExpenseChart expenses={expenseList} />
         </div>
-
       </div>
     </div>
   );
