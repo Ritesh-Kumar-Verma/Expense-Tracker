8c837e9cd81518d016e2d0d2cc6ddeab3793befe
Ritesh Kumar Verma <67832781+Ritesh-Kumar-Verma@users.noreply.github.com>
2025-07-28 21:05:22 +0530

    fixed sidebar active issue

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -7,15 +7,14 @@ const Sidebar = ({tab,openTab}) => {
       <div className="sidebar">
         <div className="profile">
           <img src={assets.boy} alt="" />
-          {/* <p>Expense Tracker</p> */}
         </div>
 
         <div className="menu">
-          <div className="home menu-item" onClick={()=>{openTab("Home");}}>
+          <div className={`home menu-item ${(tab === "Home")?"active":""}`} onClick={()=>{openTab("Home");}}>
             <img src={assets.home}  alt="" />
             <p>Home</p>
           </div>
-          <div className="expense menu-item" onClick={()=>{openTab("ExpensesSetting");}}>
+          <div className={`expense menu-item ${(tab === 'ExpensesSetting')? "active": ""}`} onClick={()=>{openTab("ExpensesSetting");}}>
             <img src={assets.expense} alt="" />
             <p>Expense</p>
           </div>
@@ -26,3 +25,7 @@ const Sidebar = ({tab,openTab}) => {
 };
 
 export default Sidebar;
+
+
+
+// "home menu-item "
